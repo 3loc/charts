dc9c97d0e3e5e6da1c59e2c573e89d9d2ec8a6b4
Ted Östrem <ted@3loc.ltd>
2019-11-27 11:47:50 +0800

    Added functionality for mlflow to wait for a folder to be created before starting service

---
+++ mlflow/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu
+
+if [[ "${WAIT_FOR_DATA_FOLDER}" ]]; then
+	echo -n "Waiting for ${WAIT_FOR_DATA_FOLDER} to be created..."
+	while [[ ! -d "${WAIT_FOR_DATA_FOLDER}" ]]; do
+		echo -n "."
+		sleep 1;
+	done
+fi
+
+mlflow "$@"
